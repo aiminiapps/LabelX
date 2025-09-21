c808aadc91988febdcb1e72f861798a8124dad99
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-09-21 23:04:58 +0530

    storage name changed

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -245,7 +245,7 @@ export const useStore = create(
       },
     }),
     {
-      name: 'DNAU-storage',
+      name: 'Lablex-storage',
       storage: createJSONStorage(() => safeStorage),
       // Optimize persistence with partialize to reduce storage writes
       partialize: (state) => ({
