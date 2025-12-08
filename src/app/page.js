054ee77f400d129b400a3ef2484f6b3f5a4a29fd
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-12-08 18:19:16 +0530

    top nav mt added for the iframe

--- src/app/page.js
+++ src/app/page.js
@@ -556,7 +556,7 @@ function TelegramMiniApp() {
     const statusDisplay = getWalletStatusDisplay();
   
     return (
-      <div className="relative">
+      <div className="relative sm:mt-7 mt-0">
         <div className="w-full flex justify-between items-center pb-3 px-1">
           <div className='flex items-center gap-3'>
             <Image 
@@ -667,7 +667,7 @@ function TelegramMiniApp() {
   
   
   const renderHomeContent = () => (
-    <div className="space-y-">
+    <div className="">
       <EarnRewardsBanner/>
       <LabelXNetworkGlobe/>
       <DataCenterHome />
